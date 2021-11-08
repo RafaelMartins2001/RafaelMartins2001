033b43040c454c175ba6dba7cb37fb9a3124b2af
RafaelMartins2001 <65284291+RafaelMartins2001@users.noreply.github.com>
2021-11-08 12:44:02 +0000

    Part2_AssyncAwait_Update

--- Exercicios/Parte2/aplicationAssyncAwait.js
+++ Exercicios/Parte2/aplicationAssyncAwait.js
@@ -10,26 +10,34 @@ const props =['name', 'id', 'url'];
 const ATLAS_CLIENT_ID = process.env['ATLAS_CLIENT_ID']
     
 
-async function getWithAsyncAwait(url) {
+async function getWithAsyncAwait(...url) {
+	let final_result = new Array;
+	for(let i in url){
 	try {
-		const res = await fetch(
-			`${url}${ATLAS_CLIENT_ID}`
+		
+		let res = await fetch(
+			`${url[i]}${ATLAS_CLIENT_ID}`
 		);
 		if (res.status === 200) {		
 			console.log(res.status, res.statusText);
-			const txt = await res.text();
-         const obj = JSON.parse(txt);
-		   const object = filterProperties(props, obj.lists);
-		   const result = JSON.stringify(object)
+			let txt = await res.text();
+         let obj = JSON.parse(txt);
+		   let object = filterProperties(props, obj.lists);
+		   let result = JSON.stringify(object)
          console.log("written to file");
-		   return fsp.appendFile(filename, result);
+		   final_result.push(result);
 		} else {
 			throw { msg: "Invalid response", err: res };
 		}
+		
+		
 	} catch (err) {
 		console.log("[CATCH]", err);
 	}
 }
+return fsp.appendFile(filename, final_result);
+}
 
 const url = 'https://api.boardgameatlas.com/api/lists?username=trentellingsen&client_id=';
-getWithAsyncAwait(url);
+const url2 = 'https://api.boardgameatlas.com/api/lists?username=trentellingsen&client_id=';
+getWithAsyncAwait(url, url2);
